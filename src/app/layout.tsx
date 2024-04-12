ffc8fb64b6a983357d7adc7f1acf43b3a0c43605
Matthew Goluba <mkgoluba@outlook.com>
2024-04-12 16:57:48 -0500

    fix eslint issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import ThemeRegistry from './ThemeRegistry'
 
-import React from 'react';
+import React from 'react'
 
 export default function RootLayout({
   children,
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body style={{ height: '100vh', backgroundColor: '#63B3DD' }}>
         <AppRouterCacheProvider>
-            <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry>
+          <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry>
         </AppRouterCacheProvider>
       </body>
     </html>
