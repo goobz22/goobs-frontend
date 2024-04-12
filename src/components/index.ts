d73648a0b0bce7d6eaeb6bdbca327e0dc6351193
Matthew Goluba <mkgoluba@outlook.com>
2024-04-12 18:01:09 -0500

    configure component package structure to be as expected

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CustomButton } from './Button';
+export { default as ConfirmationCodeInputs } from './ConfirmationCodeInput';
+export { default as CustomGrid } from './Grid';
+export { default as Typography } from './Typography';
