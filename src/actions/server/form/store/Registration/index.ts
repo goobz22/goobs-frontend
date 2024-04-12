c80158ff8bfd6e4b6683e8477b28c4c8b81f81ce
Matthew Goluba <mkgoluba@outlook.com>
2024-04-12 15:21:18 -0500

    prettied things up

--- src/actions/server/form/store/Registration/index.ts
+++ src/actions/server/form/store/Registration/index.ts
@@ -1,66 +1,98 @@
-'use server';
-import { ValidationProps, SpreadErrorMessage } from '@/types/validation';
-import { createDataField } from '../dataField';
+'use server'
+import { ValidationProps, SpreadErrorMessage } from '@/types/validation'
+import { createDataField } from '../dataField'
 
 class RegistrationStore {
-  private static instance: RegistrationStore | null = null;
-  private store: Record<string, Record<string, any>> = {};
+  private static instance: RegistrationStore | null = null
+  private store: Record<string, Record<string, any>> = {}
 
   private constructor() {
-    console.log('RegistrationStore constructor called');
+    console.log('RegistrationStore constructor called')
   }
 
   public static async getInstance(): Promise<RegistrationStore> {
-    console.log('Getting RegistrationStore instance');
+    console.log('Getting RegistrationStore instance')
     if (!RegistrationStore.instance) {
-      RegistrationStore.instance = new RegistrationStore();
+      RegistrationStore.instance = new RegistrationStore()
     }
-    console.log('RegistrationStore instance retrieved');
-    return RegistrationStore.instance;
+    console.log('RegistrationStore instance retrieved')
+    return RegistrationStore.instance
   }
 
   public async setRegistrationToken(registrationToken: string): Promise<void> {
-    console.log('Setting registration token:', registrationToken);
-    this.store[registrationToken] = {};
-    console.log('Registration token set');
+    console.log('Setting registration token:', registrationToken)
+    this.store[registrationToken] = {}
+    console.log('Registration token set')
   }
 
-  public async getRegistrationToken(registrationToken: string): Promise<SpreadErrorMessage | undefined> {
-    console.log('Getting registration token for registration token:', registrationToken);
-    const result = this.store[registrationToken]?.['registrationToken'];
-    console.log('RegistrationToken retrieved with identifier:', registrationToken, 'and value:', result);
+  public async getRegistrationToken(
+    registrationToken: string
+  ): Promise<SpreadErrorMessage | undefined> {
+    console.log(
+      'Getting registration token for registration token:',
+      registrationToken
+    )
+    const result = this.store[registrationToken]?.['registrationToken']
+    console.log(
+      'RegistrationToken retrieved with identifier:',
+      registrationToken,
+      'and value:',
+      result
+    )
     if (result && result.identifier === registrationToken) {
-      return result;
+      return result
     }
-    return undefined;
+    return undefined
   }
 
-  public async get(registrationToken: string, identifier: string): Promise<SpreadErrorMessage | undefined> {
-    console.log('Getting value for registration token:', registrationToken, 'and identifier:', identifier);
-    const result = this.store[registrationToken]?.[identifier];
-    console.log('Value retrieved with identifier:', identifier, 'and value:', result);
+  public async get(
+    registrationToken: string,
+    identifier: string
+  ): Promise<SpreadErrorMessage | undefined> {
+    console.log(
+      'Getting value for registration token:',
+      registrationToken,
+      'and identifier:',
+      identifier
+    )
+    const result = this.store[registrationToken]?.[identifier]
+    console.log(
+      'Value retrieved with identifier:',
+      identifier,
+      'and value:',
+      result
+    )
     if (result && result.identifier === identifier) {
-      return result;
+      return result
     }
-    return undefined;
+    return undefined
   }
 
-  public async set(registrationToken: string, value: string, identifier: string): Promise<void> {
-    console.log('Setting value for registration token:', registrationToken, 'and identifier:', identifier);
+  public async set(
+    registrationToken: string,
+    value: string,
+    identifier: string
+  ): Promise<void> {
+    console.log(
+      'Setting value for registration token:',
+      registrationToken,
+      'and identifier:',
+      identifier
+    )
     if (!this.store[registrationToken]) {
-      this.store[registrationToken] = {};
+      this.store[registrationToken] = {}
     }
-    const existingValue = this.store[registrationToken][identifier];
+    const existingValue = this.store[registrationToken][identifier]
     if (existingValue && existingValue.identifier !== identifier) {
-      console.log('Identifier does not match, not updating the value');
-      return;
+      console.log('Identifier does not match, not updating the value')
+      return
     }
-    this.store[registrationToken][identifier] = { identifier, value };
-    console.log('Value set with identifier:', identifier, 'and value:', value);
+    this.store[registrationToken][identifier] = { identifier, value }
+    console.log('Value set with identifier:', identifier, 'and value:', value)
   }
 }
 
 export async function getRegistrationStore(): Promise<RegistrationStore> {
-  console.log('Exporting RegistrationStore instance');
-  return RegistrationStore.getInstance();
+  console.log('Exporting RegistrationStore instance')
+  return RegistrationStore.getInstance()
 }
--- src/actions/server/form/store/dataField.ts
+++ src/actions/server/form/store/dataField.ts
@@ -1,97 +1,106 @@
-'use server';
-import { HelperFooterMessage, ValidationProps, SpreadErrorMessage } from '@/types/validation';
-import { createEncryptionUtility, decryptValue } from './crypt';
+'use server'
+import {
+  HelperFooterMessage,
+  ValidationProps,
+  SpreadErrorMessage,
+} from '../../../../types/validation/index'
+import { createEncryptionUtility, decryptValue } from './crypt'
 
 class DataField<T extends HelperFooterMessage & SpreadErrorMessage> {
-  private store: Record<string, T>;
-  private identifier: string | null;
+  private store: Record<string, T>
+  private identifier: string | null
 
   constructor(store: Record<string, T>) {
-    console.log('DataField constructor called');
-    this.store = store;
-    this.identifier = null;
+    console.log('DataField constructor called')
+    this.store = store
+    this.identifier = null
   }
 
   async get(props: HelperFooterMessage): Promise<T | undefined> {
-    console.log('DataField get called with props:', props);
+    console.log('DataField get called with props:', props)
     if (!this.identifier) {
-      console.error('Identifier not set');
-      throw new Error('Identifier not set. Call setIdentifier() before using get().');
+      console.error('Identifier not set')
+      throw new Error(
+        'Identifier not set. Call setIdentifier() before using get().'
+      )
     }
-    console.log('Getting data with identifier:', this.identifier);
+    console.log('Getting data with identifier:', this.identifier)
     if (this.identifier && this.store[this.identifier]) {
-      const storedData = this.store[this.identifier];
-      console.log('Stored data retrieved:', storedData);
+      const storedData = this.store[this.identifier]
+      console.log('Stored data retrieved:', storedData)
       if (storedData.value && typeof storedData.value === 'string') {
-        const decryptedValue = await decryptValue(storedData.value);
-        console.log('Decrypted value:', decryptedValue);
-        return { ...storedData, value: decryptedValue };
+        const decryptedValue = await decryptValue(storedData.value)
+        console.log('Decrypted value:', decryptedValue)
+        return { ...storedData, value: decryptedValue }
       }
-      return storedData;
+      return storedData
     }
-    console.log('Data not found for identifier:', this.identifier);
-    return undefined;
+    console.log('Data not found for identifier:', this.identifier)
+    return undefined
   }
 
   async set(props: ValidationProps & T): Promise<void> {
-    console.log('DataField set called with props:', props);
+    console.log('DataField set called with props:', props)
     if (!this.identifier) {
-      console.error('Identifier not set');
-      throw new Error('Identifier not set. Call setIdentifier() before using set().');
+      console.error('Identifier not set')
+      throw new Error(
+        'Identifier not set. Call setIdentifier() before using set().'
+      )
     }
 
-    const value = props.value;
+    const value = props.value
     if (value === undefined || typeof value !== 'string') {
-      console.error('Invalid value. Expected a string.');
-      throw new Error('Invalid value. Expected a string.');
+      console.error('Invalid value. Expected a string.')
+      throw new Error('Invalid value. Expected a string.')
     }
 
-    const encryptionUtility = await createEncryptionUtility();
-    console.log('Original value before encryption:', value);
-    const encryptedValue = encryptionUtility.encrypt(value);
-    console.log('Encrypted value:', encryptedValue);
+    const encryptionUtility = await createEncryptionUtility()
+    console.log('Original value before encryption:', value)
+    const encryptedValue = encryptionUtility.encrypt(value)
+    console.log('Encrypted value:', encryptedValue)
 
     let data: Record<string, any> = {
-      'identifier': this.identifier,
-      'value': encryptedValue,
-    };
+      identifier: this.identifier,
+      value: encryptedValue,
+    }
 
-    console.log('Storing data with identifier:', this.identifier);
-    this.store[this.identifier] = data as T;
-    console.log('Stored data:', this.store[this.identifier]);
+    console.log('Storing data with identifier:', this.identifier)
+    this.store[this.identifier] = data as T
+    console.log('Stored data:', this.store[this.identifier])
   }
 
   async delete(): Promise<void> {
-    console.log('DataField delete called');
+    console.log('DataField delete called')
     if (!this.identifier) {
-      console.error('Identifier not set');
-      throw new Error('Identifier not set. Call setIdentifier() before using delete().');
+      console.error('Identifier not set')
+      throw new Error(
+        'Identifier not set. Call setIdentifier() before using delete().'
+      )
     }
 
-    console.log('Deleting data with identifier:', this.identifier);
-    delete this.store[this.identifier];
-    console.log('Deleted data with identifier:', this.identifier);
+    console.log('Deleting data with identifier:', this.identifier)
+    delete this.store[this.identifier]
+    console.log('Deleted data with identifier:', this.identifier)
   }
 
   async setIdentifier(identifier: string): Promise<void> {
-    console.log('Setting identifier:', identifier);
-    this.identifier = identifier;
+    console.log('Setting identifier:', identifier)
+    this.identifier = identifier
   }
 
   getIdentifier(): string | null {
-    console.log('Getting identifier:', this.identifier);
-    return this.identifier;
+    console.log('Getting identifier:', this.identifier)
+    return this.identifier
   }
 }
 
 // Exporting a function to create instances of DataField
-export async function createDataField<T extends HelperFooterMessage & SpreadErrorMessage>(
-  store: Record<string, T>,
-  identifier: string
-): Promise<DataField<T>> {
-  console.log('Creating DataField instance with identifier:', identifier);
-  const dataField = new DataField<T>(store);
-  await dataField.setIdentifier(identifier);
-  console.log('DataField instance created');
-  return dataField;
+export async function createDataField<
+  T extends HelperFooterMessage & SpreadErrorMessage,
+>(store: Record<string, T>, identifier: string): Promise<DataField<T>> {
+  console.log('Creating DataField instance with identifier:', identifier)
+  const dataField = new DataField<T>(store)
+  await dataField.setIdentifier(identifier)
+  console.log('DataField instance created')
+  return dataField
 }
