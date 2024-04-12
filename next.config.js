65c30de8cbbcedf6f0e78311e7535944edb8a4a7
Matthew Goluba <mkgoluba@outlook.com>
2024-04-12 15:20:20 -0500

    introduce configuration of environment for project

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+// next.config.js
+
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://localhost:3000',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+export default nextConfig
