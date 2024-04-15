01b9b1a7753c83918d98460be7f90163cd4dba65
Matthew Goluba <mkgoluba@outlook.com>
2024-04-15 05:06:17 -0500

    bring over config file for update user and get user so any db and types can be used. preps us for generic get and update file

---
+++ src/config/registration/userDatabase.tsx
@@ -0,0 +1,12 @@
+// Import the database connection utility
+import connectDb from '@/functions/mongo/connectDb'
+
+// Import the user model
+import UserRegistration from '@/mongo/user/model'
+
+const userDatabase = {
+  databaseConnectionUtility: connectDb,
+  databaseUserModel: UserRegistration,
+}
+
+export default userDatabase
