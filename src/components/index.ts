f3a50f891e8cf2442dd04bbb8fbf649f5e51246d
Matthew Goluba <mkgoluba@outlook.com>
2024-05-13 00:03:32 -0500

    update to latest components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as CustomButton } from './Button'
-export { default as ConfirmationCodeInputs } from './ConfirmationCodeInput'
 export { default as CustomGrid } from './Grid'
+export { default as StyledComponent } from './StyledComponent'
 export { default as Typography } from './Typography'
