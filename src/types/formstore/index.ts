539607a7baf87c0df199e1b1c01bde76b1b3cdbb
Matthew Goluba <mkgoluba@outlook.com>
2024-05-14 13:23:44 -0500

    move types all to one folder and push .env through props instead

--- src/types/formstore/index.ts
+++ src/types/formstore/index.ts
@@ -3,4 +3,13 @@ export interface FormStoreProps {
   storename: string
   value: string
   expirationTime: number
+  encryptionKey: string
+  encryptionIV: string
+}
+
+export type ReusableStore = {
+  storageDir: string
+  get: (storename: string, identifier: string) => Promise<FormStoreProps | undefined>
+  set: (props: FormStoreProps) => Promise<void>
+  cleanup: () => Promise<void>
 }
