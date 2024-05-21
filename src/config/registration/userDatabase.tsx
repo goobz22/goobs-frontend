1dec844c10bb6a23efe95e8622e849d4275dd734
Matthew Goluba <mkgoluba@outlook.com>
2024-05-21 15:29:13 -0500

    update to reflect new file structure

--- src/config/registration/userDatabase.tsx
+++ src/config/registration/userDatabase.tsx
@@ -1,8 +1,8 @@
 // Import the database connection utility
-import connectDb from '../../functions/mongo/connectDb'
+import connectDb from '../../mongodb/utils/connectDb'
 
 // Import the user model
-import UserRegistration from '../../mongo/user/model'
+import UserRegistration from '../../mongodb/user/model'
 
 const userDatabase = {
   databaseConnectionUtility: connectDb,
