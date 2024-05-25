15a70c3407b64850cef165e4717c6bfb1474bda3
Matthew Goluba <mkgoluba@outlook.com>
2024-05-25 10:50:22 -0500

    make imports more flexible minor update to support and fix build

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,186 @@
 // Components
-export { default as CustomButton } from './components/Button'
-export { default as CustomGrid } from './components/Grid'
-export { default as StyledComponent } from './components/StyledComponent'
-export { default as Typography } from './components/Typography'
+import CustomButton from './components/Button'
+import CustomGrid from './components/Grid'
+import StyledComponent from './components/StyledComponent'
+import Typography from './components/Typography'
 
 // Actions
-export {
-  getReusableStore,
-  setReusableStore,
-} from './actions/server/form/store/reusableStore'
-export { encryptValue, decryptValue } from './actions/server/form/store/crypt'
-export { default as getFormData } from './actions/server/form/getFormData'
-
-// Types
-export * from './types/button'
-export * from './types/content/alignment'
-export * from './types/content/animation'
-export * from './types/formstore'
-export * from './types/grid/customgrid'
-export * from './types/styledcomponent'
-export * from './types/typography'
+import { reusableStore } from './actions/server/form/store/reusableStore'
+import { encryptValue, decryptValue } from './actions/server/form/store/crypt'
+import getFormData from './actions/server/form/getFormData'
 
 // Colors
-export { moss } from './styles/palette'
-export { aqua } from './styles/palette'
-export { madder } from './styles/palette'
-export { woad } from './styles/palette'
-export { marine } from './styles/palette'
-export { pansy } from './styles/palette'
-export { stainlessSteel } from './styles/palette'
-export { coal } from './styles/palette'
-export { ocean } from './styles/palette'
-export { sky } from './styles/palette'
-export { salmon } from './styles/palette'
-export { lightning } from './styles/palette'
-export { sage } from './styles/palette'
-export { lilac } from './styles/palette'
-export { gunpowder } from './styles/palette'
-export { lightMadder } from './styles/palette'
-export { black } from './styles/palette'
-export { white } from './styles/palette'
-export { none } from './styles/palette'
-export { semiTransparentWhite } from './styles/palette'
-export { semiTransparentBlack } from './styles/palette'
-export { red } from './styles/palette'
-export { green } from './styles/palette'
-export { greyborder } from './styles/palette'
+import {
+  moss,
+  aqua,
+  madder,
+  woad,
+  marine,
+  pansy,
+  stainlessSteel,
+  coal,
+  ocean,
+  sky,
+  salmon,
+  lightning,
+  sage,
+  lilac,
+  gunpowder,
+  lightMadder,
+  black,
+  white,
+  none,
+  semiTransparentWhite,
+  semiTransparentBlack,
+  red,
+  green,
+  greyborder,
+} from './styles/palette'
 
 // Typography
-export { arapeyh1 } from './styles/typography'
-export { arapeyh2 } from './styles/typography'
-export { arapeyh3 } from './styles/typography'
-export { arapeyh4 } from './styles/typography'
-export { arapeyh5 } from './styles/typography'
-export { arapeyh6 } from './styles/typography'
-export { arapeyparagraph } from './styles/typography'
-export { interh1 } from './styles/typography'
-export { interh2 } from './styles/typography'
-export { interh3 } from './styles/typography'
-export { interh4 } from './styles/typography'
-export { interh5 } from './styles/typography'
-export { interh6 } from './styles/typography'
-export { interparagraph } from './styles/typography'
-export { interhelperheader } from './styles/typography'
-export { interhelperfooter } from './styles/typography'
-export { merrih1 } from './styles/typography'
-export { merrih2 } from './styles/typography'
-export { merrih3 } from './styles/typography'
-export { merrih4 } from './styles/typography'
-export { merrih5 } from './styles/typography'
-export { merrih6 } from './styles/typography'
-export { merriparagraph } from './styles/typography'
-export { merrihelperfooter } from './styles/typography'
+import {
+  arapeyh1,
+  arapeyh2,
+  arapeyh3,
+  arapeyh4,
+  arapeyh5,
+  arapeyh6,
+  arapeyparagraph,
+  interh1,
+  interh2,
+  interh3,
+  interh4,
+  interh5,
+  interh6,
+  interparagraph,
+  interhelperheader,
+  interhelperfooter,
+  merrih1,
+  merrih2,
+  merrih3,
+  merrih4,
+  merrih5,
+  merrih6,
+  merriparagraph,
+  merrihelperfooter,
+} from './styles/typography'
+
+// Named exports
+export {
+  CustomButton,
+  CustomGrid,
+  StyledComponent,
+  Typography,
+  reusableStore,
+  encryptValue,
+  decryptValue,
+  getFormData,
+  moss,
+  aqua,
+  madder,
+  woad,
+  marine,
+  pansy,
+  stainlessSteel,
+  coal,
+  ocean,
+  sky,
+  salmon,
+  lightning,
+  sage,
+  lilac,
+  gunpowder,
+  lightMadder,
+  black,
+  white,
+  none,
+  semiTransparentWhite,
+  semiTransparentBlack,
+  red,
+  green,
+  greyborder,
+  arapeyh1,
+  arapeyh2,
+  arapeyh3,
+  arapeyh4,
+  arapeyh5,
+  arapeyh6,
+  arapeyparagraph,
+  interh1,
+  interh2,
+  interh3,
+  interh4,
+  interh5,
+  interh6,
+  interparagraph,
+  interhelperheader,
+  interhelperfooter,
+  merrih1,
+  merrih2,
+  merrih3,
+  merrih4,
+  merrih5,
+  merrih6,
+  merriparagraph,
+  merrihelperfooter,
+}
+
+// Default export
+export default {
+  CustomButton,
+  CustomGrid,
+  StyledComponent,
+  Typography,
+  reusableStore,
+  encryptValue,
+  decryptValue,
+  getFormData,
+  moss,
+  aqua,
+  madder,
+  woad,
+  marine,
+  pansy,
+  stainlessSteel,
+  coal,
+  ocean,
+  sky,
+  salmon,
+  lightning,
+  sage,
+  lilac,
+  gunpowder,
+  lightMadder,
+  black,
+  white,
+  none,
+  semiTransparentWhite,
+  semiTransparentBlack,
+  red,
+  green,
+  greyborder,
+  arapeyh1,
+  arapeyh2,
+  arapeyh3,
+  arapeyh4,
+  arapeyh5,
+  arapeyh6,
+  arapeyparagraph,
+  interh1,
+  interh2,
+  interh3,
+  interh4,
+  interh5,
+  interh6,
+  interparagraph,
+  interhelperheader,
+  interhelperfooter,
+  merrih1,
+  merrih2,
+  merrih3,
+  merrih4,
+  merrih5,
+  merrih6,
+  merriparagraph,
+  merrihelperfooter,
+}
