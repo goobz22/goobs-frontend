819b58159cd8fba7ea1182e29e5934b180791a78
Matthew Goluba <mkgoluba@outlook.com>
2024-07-14 16:09:25 -0500

    update to fix usepassword hook

--- src/components/Icons/ShowHideEye.tsx
+++ src/components/Icons/ShowHideEye.tsx
@@ -1,10 +1,16 @@
-// src/components/Icons/ShowHideEyeIcon.tsx
+'use client'
 
 import React from 'react'
 import VisibilityIcon from '@mui/icons-material/Visibility'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 
-const ShowHideEyeIcon = ({ visible }: { visible: boolean }) => {
+interface ShowHideEyeIconProps {
+  visible?: boolean
+}
+
+const ShowHideEyeIcon: React.FC<ShowHideEyeIconProps> = ({
+  visible = false,
+}) => {
   const iconStyle = { color: 'black' }
   return visible ? (
     <VisibilityIcon style={iconStyle} />
