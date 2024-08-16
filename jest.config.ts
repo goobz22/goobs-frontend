de5438814bbc78c5874778bcdc717f7ba38e11e7
Matthew Goluba <mkgoluba@outlook.com>
2024-08-16 02:59:42 -0500

    move jest config to typescript

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default config
