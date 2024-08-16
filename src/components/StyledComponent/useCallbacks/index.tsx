4b8d2d7a309bb98553b2120d6a0a288a6d034fff
Matthew Goluba <mkgoluba@outlook.com>
2024-08-16 03:01:27 -0500

    make useeffects usecallback

--- src/components/StyledComponent/useCallbacks/index.tsx
+++ src/components/StyledComponent/useCallbacks/index.tsx
@@ -1,25 +1,21 @@
-import React, { useEffect } from 'react'
+import { useCallback } from 'react'
 
 /**
  * Custom hook that tracks whether an input field has a value or a specific status.
  *
  * @param {string | undefined} value - The current value of the input field.
  * @param {boolean | undefined} valuestatus - A boolean status associated with the input field.
- * @param {React.Dispatch<React.SetStateAction<boolean>>} setHasInput - State setter function to update the hasInput state.
+ *
+ * @returns {boolean} - Whether the input field has a value or a specific status.
  *
  * @example
- * const [hasInput, setHasInput] = useState(false);
- * useHasInputEffect(inputValue, inputStatus, setHasInput);
+ * const hasInput = useHasInput(inputValue, inputStatus);
  */
-export const useHasInputEffect = (
+export const useHasInput = (
   value: string | undefined,
-  valuestatus: boolean | undefined,
-  setHasInput: React.Dispatch<React.SetStateAction<boolean>>
-) => {
-  useEffect(() => {
-    const hasInput = !!value || !!valuestatus
-    setHasInput(hasInput)
-  }, [value, valuestatus, setHasInput])
+  valuestatus: boolean | undefined
+): boolean => {
+  return !!value || !!valuestatus
 }
 
 /**
@@ -28,22 +24,23 @@ export const useHasInputEffect = (
  *
  * @param {React.RefObject<HTMLInputElement>} inputRefInternal - React ref object for the input element.
  *
+ * @returns {(input: HTMLInputElement | null) => void} - A callback function to set the attributes on the input element.
+ *
  * @example
  * const inputRef = useRef<HTMLInputElement>(null);
- * usePreventAutocompleteEffect(inputRef);
+ * const setInputAttributes = usePreventAutocomplete();
  * // In your JSX:
- * <input ref={inputRef} ... />
+ * <input ref={el => { inputRef.current = el; setInputAttributes(el); }} ... />
  */
-export const usePreventAutocompleteEffect = (
-  inputRefInternal: React.RefObject<HTMLInputElement>
-) => {
-  useEffect(() => {
-    const input = inputRefInternal.current
+export const usePreventAutocomplete = (): ((
+  input: HTMLInputElement | null
+) => void) => {
+  return useCallback((input: HTMLInputElement | null) => {
     if (input) {
       input.setAttribute('autocomplete', 'new-password')
       input.setAttribute('autocorrect', 'off')
       input.setAttribute('autocapitalize', 'none')
       input.setAttribute('spellcheck', 'false')
     }
-  }, [inputRefInternal])
+  }, [])
 }
