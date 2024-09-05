18c62e5711e70592ee6ff2cbac2e5c31e8a0e9f7
Matthew Goluba <mkgoluba@outlook.com>
2024-09-05 16:34:50 -0500

    update dropdown to work with formnik and zod as expected

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -4,19 +4,73 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
-  SelectChangeEvent,
+  SelectProps,
+  FormHelperText,
 } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-export interface DropdownProps {
+export interface DropdownOption {
+  value: string
   label: string
-  options: string[]
-  defaultOption?: string
-  onChange?: () => void
+}
+
+export interface DropdownProps extends Omit<SelectProps, 'onChange'> {
+  /**
+   * The label for the dropdown.
+   */
+  label: string
+  /**
+   * The options for the dropdown.
+   */
+  options: DropdownOption[]
+  /**
+   * The default value for the dropdown.
+   */
+  defaultValue?: string
+  /**
+   * The background color of the dropdown.
+   */
   backgroundcolor?: string
+  /**
+   * The outline color of the dropdown.
+   */
   outlinecolor?: string
+  /**
+   * The font color of the dropdown.
+   */
   fontcolor?: string
+  /**
+   * The font color of the dropdown label when shrunk.
+   */
   shrunkfontcolor?: string
+  /**
+   * Callback function triggered when the dropdown value changes.
+   */
+  onChange?: SelectProps['onChange']
+  /**
+   * Indicates if the dropdown is in an error state.
+   */
+  error?: boolean
+  /**
+   * The helper text to display below the dropdown.
+   */
+  helperText?: string
+  /**
+   * The name of the dropdown.
+   */
+  name?: string
+  /**
+   * Indicates if the dropdown is required.
+   */
+  required?: boolean
+  /**
+   * Callback function triggered when the dropdown loses focus.
+   */
+  onBlur?: SelectProps['onBlur']
+  /**
+   * Callback function triggered when the dropdown receives focus.
+   */
+  onFocus?: SelectProps['onFocus']
 }
 
 const StyledFormControl = styled(FormControl)<{
@@ -46,44 +100,74 @@ const StyledInputLabel = styled(InputLabel)<{ shrunkfontcolor?: string }>(
   })
 )
 
+/**
+ * Dropdown component built with Material UI.
+ */
 const Dropdown: React.FC<DropdownProps> = ({
   label,
   options,
-  defaultOption,
-  onChange,
+  defaultValue,
   backgroundcolor,
   outlinecolor,
   fontcolor,
   shrunkfontcolor,
+  onChange,
+  error = false,
+  helperText,
+  name,
+  required = false,
+  onBlur,
+  onFocus,
+  ...rest
 }) => {
-  const [selectedValue, setSelectedValue] = useState(defaultOption || '')
+  const [selectedValue, setSelectedValue] = useState(() => {
+    const defaultOption = options.find(option => option.value === defaultValue)
+    return defaultOption ? defaultOption.value : ''
+  })
 
-  const handleChange = (event: SelectChangeEvent) => {
+  const handleChange: SelectProps['onChange'] = (event, child) => {
     const newValue = event.target.value as string
     setSelectedValue(newValue)
-    onChange?.()
+    onChange?.(event, child)
+  }
+
+  const handleBlur: SelectProps['onBlur'] = event => {
+    onBlur?.(event)
+  }
+
+  const handleFocus: SelectProps['onFocus'] = event => {
+    onFocus?.(event)
   }
 
   return (
     <StyledFormControl
       backgroundcolor={backgroundcolor}
       outlinecolor={outlinecolor}
+      error={error}
+      required={required}
     >
-      <StyledInputLabel shrunkfontcolor={shrunkfontcolor}>
+      <StyledInputLabel id={`${name}-label`} shrunkfontcolor={shrunkfontcolor}>
         {label}
       </StyledInputLabel>
       <Select
+        labelId={`${name}-label`}
         value={selectedValue}
         onChange={handleChange}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
         label={label}
         sx={{ color: fontcolor }}
+        name={name}
+        aria-labelledby={`${name}-label`}
+        {...rest}
       >
         {options.map(option => (
-          <MenuItem key={option} value={option}>
-            {option}
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
           </MenuItem>
         ))}
       </Select>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
     </StyledFormControl>
   )
 }
