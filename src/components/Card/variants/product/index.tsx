a8e600cc8956f3cb251c7afcd714203335d9e371
Matthew Goluba <mkgoluba@outlook.com>
2024-10-21 01:08:54 -0500

    force use client

--- src/components/Card/variants/product/index.tsx
+++ src/components/Card/variants/product/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react'
 import { Box, Paper, BoxProps } from '@mui/material'
 import Typography from '../../../../components/Typography'
--- src/components/Card/variants/productsummary/index.tsx
+++ src/components/Card/variants/productsummary/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react'
 import { Box, Paper, Switch } from '@mui/material'
 import { CardProps } from '../../index'
