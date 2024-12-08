0666c481882fa71e05b4d6a8dcf1697658230749
Matthew Goluba <mkgoluba@outlook.com>
2024-12-08 16:32:57 -0600

    fix build issues on react 19

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,44 +1,83 @@
 'use client'
 
-import React from 'react'
+import React, { JSX } from 'react'
 import { styled } from '@mui/material/styles'
 import MuiAccordion, { AccordionProps } from '@mui/material/Accordion'
 import MuiAccordionSummary, {
   AccordionSummaryProps,
 } from '@mui/material/AccordionSummary'
-import MuiAccordionDetails from '@mui/material/AccordionDetails'
+import MuiAccordionDetails, {
+  AccordionDetailsProps,
+} from '@mui/material/AccordionDetails'
 import { ExpandMore } from '@mui/icons-material'
 import { black } from '../../styles/palette'
 
-/**
- * Custom styled Accordion component.
- * Wraps MUI's Accordion with custom styling and disables gutters by default.
- */
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters {...props} />
-))({})
+type CustomAccordionProps = Omit<AccordionProps, 'children'> & {
+  children: NonNullable<React.ReactNode>
+}
 
-/**
- * Custom styled AccordionSummary component.
- * Wraps MUI's AccordionSummary with custom styling and a custom expand icon.
- */
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    expandIcon={<ExpandMore sx={{ color: black.main }} />}
-    {...props}
-  />
-))({
+type CustomAccordionSummaryProps = Omit<AccordionSummaryProps, 'children'> & {
+  children: NonNullable<React.ReactNode>
+}
+
+type CustomAccordionDetailsProps = Omit<AccordionDetailsProps, 'children'> & {
+  children: NonNullable<React.ReactNode>
+}
+
+const StyledAccordion = styled(MuiAccordion)({
+  '&.MuiAccordion-root': {
+    '&:before': {
+      display: 'none',
+    },
+  },
+})
+
+const StyledAccordionSummary = styled(MuiAccordionSummary)({
   fontSize: '20px',
   fontFamily: 'merriweather',
   fontWeight: 500,
 })
 
-/**
- * Custom styled AccordionDetails component.
- * Wraps MUI's AccordionDetails with custom padding based on the theme's spacing.
- */
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+const StyledAccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
 }))
 
+function Accordion({ children, ...props }: CustomAccordionProps): JSX.Element {
+  return (
+    <StyledAccordion disableGutters {...props}>
+      {children}
+    </StyledAccordion>
+  )
+}
+
+function AccordionSummary({
+  children,
+  ...props
+}: CustomAccordionSummaryProps): JSX.Element {
+  return (
+    <StyledAccordionSummary
+      expandIcon={<ExpandMore sx={{ color: black.main }} />}
+      {...props}
+    >
+      {children}
+    </StyledAccordionSummary>
+  )
+}
+
+function AccordionDetails({
+  children,
+  ...props
+}: CustomAccordionDetailsProps): JSX.Element {
+  return <StyledAccordionDetails {...props}>{children}</StyledAccordionDetails>
+}
+
+Accordion.displayName = 'Accordion'
+AccordionSummary.displayName = 'AccordionSummary'
+AccordionDetails.displayName = 'AccordionDetails'
+
 export { Accordion, AccordionSummary, AccordionDetails }
+export type {
+  CustomAccordionProps,
+  CustomAccordionSummaryProps,
+  CustomAccordionDetailsProps,
+}
