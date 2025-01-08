df28653de5e3aac870b1734e91d04f3f16eac5fa
Matthew Goluba <mkgoluba@outlook.com>
2025-01-08 15:12:49 -0600

    remove unused icon

--- src/components/Icons/DownArrowFilled.tsx
+++
@@ -1,9 +0,0 @@
-// BlackArrowDropDownIcon.tsx
-import React from 'react'
-import ArrowDropDown from '@mui/icons-material/ArrowDropDown'
-
-const BlackArrowDropDownIcon: React.FC = () => {
-  return <ArrowDropDown style={{ color: 'black' }} />
-}
-
-export default BlackArrowDropDownIcon
