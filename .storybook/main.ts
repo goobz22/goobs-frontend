acc6f44d3c4aafd77a48a12d16b779e92392b470
Matthew Goluba <mkgoluba@outlook.com>
2025-02-24 00:45:44 -0600

    cleanup usage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,14 +3,25 @@ import type { StorybookConfig } from '@storybook/nextjs'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+  env: config => {
+    if (!config) return { GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '' }
+    return {
+      ...config,
+      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '',
+    }
+  },
+  docs: {
+    autodocs: 'tag',
+  },
 }
+
 export default config
