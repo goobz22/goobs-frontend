d7db00ddc8fd7aae2acacdc78c3463d1b775c5b9
Matthew Goluba <mkgoluba@outlook.com>
2025-03-17 03:52:31 -0500

    make visible on any background unselected

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Checkbox } from '@mui/material'
+import { Checkbox, styled } from '@mui/material'
 import React from 'react'
 import * as palette from '../../styles/palette'
 
@@ -11,6 +11,40 @@ export interface CheckboxProps {
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   disabled?: boolean
 }
+
+// Create a styled version of MUI Checkbox with our custom styles
+const StyledCheckbox = styled(Checkbox)(() => ({
+  padding: '8px',
+  '& .MuiSvgIcon-root': {
+    fontSize: '24px',
+  },
+  // Style for unchecked state
+  '&:not(.Mui-checked):not(.Mui-indeterminate):not(.Mui-disabled)': {
+    '& .MuiSvgIcon-root': {
+      color: 'transparent',
+      // Add a visible background to make it obvious it's a checkbox
+      backgroundColor: palette.grey.light,
+      border: `2px solid ${palette.marine.main}`,
+      borderRadius: '3px',
+    },
+  },
+  '&.Mui-checked .MuiSvgIcon-root': {
+    color: palette.marine.main,
+    border: 'none',
+  },
+  '&.Mui-indeterminate .MuiSvgIcon-root': {
+    color: palette.marine.main,
+    border: 'none',
+  },
+  '&.Mui-disabled .MuiSvgIcon-root': {
+    color: palette.grey.main,
+    border: `2px solid ${palette.grey.main}`,
+    borderRadius: '3px',
+  },
+  '&:hover': {
+    backgroundColor: `${palette.marine.light}33`,
+  },
+}))
 
 function CustomCheckbox({
   onClick,
@@ -35,28 +69,13 @@ function CustomCheckbox({
   }
 
   return (
-    <Checkbox
-      sx={{
-        color: palette.marine.main,
-        '&.Mui-checked': {
-          color: palette.marine.main,
-        },
-        '&.Mui-indeterminate': {
-          color: palette.marine.main,
-        },
-        '&.Mui-disabled': {
-          color: palette.grey.main,
-        },
-        '&:hover': {
-          backgroundColor: palette.marine.light,
-          opacity: 0.1,
-        },
-      }}
+    <StyledCheckbox
       checked={checked}
       indeterminate={indeterminate}
       onClick={handleClick}
       onChange={handleChange}
       disabled={disabled}
+      disableRipple
       {...props}
     />
   )
