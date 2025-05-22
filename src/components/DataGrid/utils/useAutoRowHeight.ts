9eae40bf596e55ad5e5c5d7154cd35f83a30b6bb
Matthew Goluba <mkgoluba@outlook.com>
2025-05-22 04:17:52 -0500

    fix lint error

--- src/components/DataGrid/utils/useAutoRowHeight.ts
+++ src/components/DataGrid/utils/useAutoRowHeight.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 /**
  * Custom hook to calculate the number of rows that can fit in the available container height
