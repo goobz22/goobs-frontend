1ff9ebf1fbee9b7a607ad953a4da7eba0312ef0f
goobz22 <mkgoluba@outlook.com>
2025-08-21 23:29:25 -0500

    remove old webpack fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,24 +11,6 @@ const config: StorybookConfig = {
     check: false,
     reactDocgen: 'react-docgen-typescript',
   },
-  webpackFinal: async config => {
-    // Disable ESLint in webpack
-    config.module?.rules?.forEach(rule => {
-      if (rule && typeof rule === 'object' && rule.use) {
-        const uses = Array.isArray(rule.use) ? rule.use : [rule.use]
-        uses.forEach(use => {
-          if (
-            typeof use === 'object' &&
-            use !== null &&
-            use.loader?.includes('eslint-loader')
-          ) {
-            rule.use = []
-          }
-        })
-      }
-    })
-    return config
-  },
 }
 
 export default config
