6847a726562b61384b821843df8d85ff3527cbb0
goobz22 <mkgoluba@outlook.com>
2025-08-29 22:31:15 -0500

    improve prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
       'no-unused-vars': 'off', // Turn off base rule as it can report incorrect errors
+      'react-hooks/exhaustive-deps': 'error',
     },
   },
 ]
