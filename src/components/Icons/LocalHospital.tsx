498cce96764414f31d61ffc9c41cb716c665f5d5
matthewgoluba <mkgoluba@outlook.com>
2025-10-04 00:04:34 -0500

    add icon

---
+++ src/components/Icons/LocalHospital.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React, { useState } from 'react'
+import {
+  IconStyles,
+  getIconStyles,
+  injectSacredKeyframes,
+  SACRED_GLYPHS,
+} from '../../theme'
+
+interface LocalHospitalIconProps extends React.SVGProps<SVGSVGElement> {
+  styles?: IconStyles
+}
+
+const LocalHospitalIcon: React.FC<LocalHospitalIconProps> = ({
+  styles,
+  style = {},
+  ...props
+}) => {
+  const [isHovered, setIsHovered] = useState(false)
+  const glyph = SACRED_GLYPHS[15]
+
+  // Inject CSS keyframes for sacred animations
+  if (styles?.theme === 'sacred') {
+    injectSacredKeyframes()
+  }
+
+  // Compute styles based on theme and state
+  const computedStyles = getIconStyles(styles, isHovered, styles?.disabled)
+
+  const iconStyle = {
+    ...computedStyles.icon,
+    ...style,
+  }
+
+  return (
+    <div
+      style={computedStyles.container}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"
+        fill="currentColor"
+        style={iconStyle}
+        {...props}
+      >
+        <path d="M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 11h-4v4h-4v-4H6v-4h4V6h4v4h4v4z" />
+      </svg>
+      {computedStyles.isSacredTheme && (
+        <div style={computedStyles.glyph}>{glyph}</div>
+      )}
+    </div>
+  )
+}
+
+export default LocalHospitalIcon
