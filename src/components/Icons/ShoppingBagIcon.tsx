733ca31a2ce8952ac918e73369c96455e3d73789
matthewgoluba <mkgoluba@outlook.com>
2025-10-17 13:11:21 -0500

    make new icon

---
+++ src/components/Icons/ShoppingBagIcon.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React, { useState } from 'react'
+import {
+  IconStyles,
+  getIconStyles,
+  injectSacredKeyframes,
+  SACRED_GLYPHS,
+} from '../../theme'
+
+interface ShoppingBagIconProps extends React.SVGProps<SVGSVGElement> {
+  styles?: IconStyles
+}
+
+const ShoppingBagIcon: React.FC<ShoppingBagIconProps> = ({
+  styles,
+  style = {},
+  ...props
+}) => {
+  const [isHovered, setIsHovered] = useState(false)
+  const glyph = SACRED_GLYPHS[8]
+
+  // Inject CSS keyframes for sacred animations
+  if (styles?.theme === 'sacred') {
+    injectSacredKeyframes()
+  }
+
+  // Compute styles based on theme and state
+  const computedStyles = getIconStyles(styles, isHovered, styles?.disabled)
+
+  const iconStyle = {
+    ...computedStyles.icon,
+    ...style,
+  }
+
+  return (
+    <div
+      style={computedStyles.container}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 -960 960 960"
+        width="24"
+        fill="currentColor"
+        style={iconStyle}
+        {...props}
+      >
+        <path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z" />
+      </svg>
+      {computedStyles.isSacredTheme && (
+        <div style={computedStyles.glyph}>{glyph}</div>
+      )}
+    </div>
+  )
+}
+
+export default ShoppingBagIcon
