3473a76ad02b1bbf8d79afac9dff30c7b6cf9c54
matthewgoluba <mkgoluba@outlook.com>
2025-10-19 20:39:40 -0500

    add icon

---
+++ src/components/Icons/ViewKanban.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React, { useState } from 'react'
+import {
+  IconStyles,
+  getIconStyles,
+  injectSacredKeyframes,
+  SACRED_GLYPHS,
+} from '../../theme'
+
+interface ViewKanbanIconProps extends React.SVGProps<SVGSVGElement> {
+  styles?: IconStyles
+}
+
+const ViewKanbanIcon: React.FC<ViewKanbanIconProps> = ({
+  styles,
+  style = {},
+  ...props
+}) => {
+  const [isHovered, setIsHovered] = useState(false)
+  const glyph = SACRED_GLYPHS[14]
+
+  // Inject CSS keyframes for sacred animations
+  if (styles?.theme === 'sacred') {
+    injectSacredKeyframes()
+  }
+
+  // Compute styles based on theme and state
+  const computedStyles = getIconStyles(styles, isHovered, styles?.disabled)
+
+  const iconStyle = {
+    ...computedStyles.icon,
+    ...style,
+  }
+
+  return (
+    <div
+      style={computedStyles.container}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"
+        fill="currentColor"
+        style={iconStyle}
+        {...props}
+      >
+        <path d="M3 3h6v9H3V3zm0 11h6v7H3v-7zm8-11h6v7h-6V3zm0 9h6v9h-6v-9zm8-9h2v18h-2V3z" />
+      </svg>
+      {computedStyles.isSacredTheme && (
+        <div style={computedStyles.glyph}>{glyph}</div>
+      )}
+    </div>
+  )
+}
+
+export default ViewKanbanIcon
